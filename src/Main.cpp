4cae3510c36722acb8341568a3100ab8ab48e708
George Fotopoulos <xorz57@gmail.com>
2024-05-15 21:16:38 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,82 @@
 #include <unordered_map>
 #include <utility>
 
+namespace xorz57 {
+    struct transition_table_hash {
+        template<class T1, class T2>
+        std::size_t operator()(const std::pair<T1, T2> &pair) const {
+            auto hash1 = std::hash<T1>{}(pair.first);
+            auto hash2 = std::hash<T2>{}(pair.second);
+            return hash1 ^ hash2;
+        }
+    };
+
+    struct transition_table_key_equal {
+        template<class T1, class T2>
+        bool operator()(const std::pair<T1, T2> &p1, const std::pair<T1, T2> &p2) const {
+            return p1.first == p2.first && p1.second == p2.second;
+        }
+    };
+
+    template<typename state_t, typename event_t>
+    using transition_table = std::unordered_map<std::pair<state_t, event_t>, std::pair<state_t, std::function<void()>>, transition_table_hash, transition_table_key_equal>;
+
+    template<typename state_t, typename event_t>
+    class state_machine {
+    public:
+        explicit state_machine(const state_t &state) : m_state(state) {}
+
+        state_machine(const state_t &state, transition_table<state_t, event_t> transition_table) : m_state(state), m_transition_table(std::move(transition_table)) {}
+
+        bool handle_event(const event_t &event) {
+            auto it = m_transition_table.find({m_state, event});
+            if (it != m_transition_table.end()) {
+                auto state = it->second.first;
+                auto transition_action = it->second.second;
+                if (m_state != state) {
+                    if (m_leave_actions.find(m_state) != m_leave_actions.end()) {
+                        m_leave_actions[m_state]();
+                    }
+                    m_state = state;
+                    transition_action();
+                    if (m_enter_actions.find(m_state) != m_enter_actions.end()) {
+                        m_enter_actions[m_state]();
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        state_t get_state() const {
+            return m_state;
+        }
+
+        void set_state(const state_t &state) {
+            m_state = state;
+        }
+
+        void set_transition_table(const transition_table<state_t, event_t> &transition_table) {
+            m_transition_table = transition_table;
+        }
+
+        void set_enter_action(const state_t &state, const std::function<void()> &enter_action) {
+            m_enter_actions[state] = enter_action;
+        }
+
+        void set_leave_action(const state_t &state, const std::function<void()> &leave_action) {
+            m_leave_actions[state] = leave_action;
+        }
+
+    private:
+        state_t m_state;
+        transition_table<state_t, event_t> m_transition_table;
+        std::unordered_map<state_t, std::function<void()>> m_enter_actions;
+        std::unordered_map<state_t, std::function<void()>> m_leave_actions;
+    };
+}// namespace xorz57
+
 enum class state {
     idle,
     running,
@@ -37,93 +113,26 @@ static std::string to_string(const event &event) {
     return "unknown";
 }
 
-namespace xorz57 {
-    struct transition_table_hash {
-        template<class T1, class T2>
-        std::size_t operator()(const std::pair<T1, T2> &pair) const {
-            auto hash1 = std::hash<T1>{}(pair.first);
-            auto hash2 = std::hash<T2>{}(pair.second);
-            return hash1 ^ hash2;
-        }
-    };
-
-    struct transition_table_key_equal {
-        template<class T1, class T2>
-        bool operator()(const std::pair<T1, T2> &p1, const std::pair<T1, T2> &p2) const {
-            return p1.first == p2.first && p1.second == p2.second;
-        }
-    };
-
-    using transition_table = std::unordered_map<std::pair<state, event>, std::pair<state, std::function<void()>>, transition_table_hash, transition_table_key_equal>;
-
-    class state_machine {
-    public:
-        explicit state_machine(const state &state) : m_state(state) {
-            std::cout << "state: " << to_string(m_state) << std::endl;
-        }
-
-        state_machine(const state &state, transition_table transition_table) : m_state(state), m_transition_table(std::move(transition_table)) {
-            std::cout << "state: " << to_string(m_state) << std::endl;
-        }
-
-        void handle_event(const event &event) {
-            auto it = m_transition_table.find({m_state, event});
-            if (it != m_transition_table.end()) {
-                auto state = it->second.first;
-                auto transition_action = it->second.second;
-                if (m_state != state) {
-                    if (m_leave_actions.find(m_state) != m_leave_actions.end()) {
-                        m_leave_actions[m_state]();
-                    }
-                    m_state = state;
-                    transition_action();
-                    if (m_enter_actions.find(m_state) != m_enter_actions.end()) {
-                        m_enter_actions[m_state]();
-                    }
-                }
-            } else {
-                std::cout << "invalid transition! state: " << to_string(m_state) << " event: " << to_string(event) << std::endl;
-            }
-        }
-
-        void set_state(const state &state) {
-            m_state = state;
-        }
-
-        void set_transition_table(const transition_table &transition_table) {
-            m_transition_table = transition_table;
-        }
-
-        void set_enter_action(const state &state, const std::function<void()> &enter_action) {
-            m_enter_actions[state] = enter_action;
-        }
-
-        void set_leave_action(const state &state, const std::function<void()> &leave_action) {
-            m_leave_actions[state] = leave_action;
-        }
-
-    private:
-        state m_state;
-        transition_table m_transition_table;
-        std::unordered_map<state, std::function<void()>> m_enter_actions;
-        std::unordered_map<state, std::function<void()>> m_leave_actions;
-    };
-}// namespace xorz57
-
 int main() {
     state s = state::idle;
 
-    xorz57::transition_table tt{
+    xorz57::transition_table<state, event> tt{
             {{state::idle, event::start}, {state::running, []() { std::cout << "transition_action: starting" << std::endl; }}},
             {{state::running, event::stop}, {state::stopped, []() { std::cout << "transition_action: stopping" << std::endl; }}},
             {{state::stopped, event::start}, {state::running, []() { std::cout << "transition_action: starting" << std::endl; }}},
     };
 
-    xorz57::state_machine sm(s, tt);
+    xorz57::state_machine<state, event> sm(s, tt);
+    std::cout << to_string(sm.get_state()) << std::endl;
 
     sm.handle_event(event::start);
+    std::cout << to_string(sm.get_state()) << std::endl;
+
     sm.handle_event(event::stop);
+    std::cout << to_string(sm.get_state()) << std::endl;
+
     sm.handle_event(event::start);
+    std::cout << to_string(sm.get_state()) << std::endl;
 
     return 0;
 }
