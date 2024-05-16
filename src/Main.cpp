494649ccf3f25f29b70a2bd233e3acc8d8a7f75e
George Fotopoulos <xorz57@gmail.com>
2024-05-16 11:21:53 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,61 +3,61 @@
 #include <iostream>
 
 enum class state {
-    idle,
-    running,
-    stopped
+    state0,
+    state1,
+    state2
 };
 
 enum class event {
-    start,
-    stop
+    event1,
+    event2
 };
 
 static std::string to_string(const state &state) {
     switch (state) {
-        case state::idle:
-            return "idle";
-        case state::running:
-            return "running";
-        case state::stopped:
-            return "stopped";
+        case state::state0:
+            return "state0";
+        case state::state1:
+            return "state1";
+        case state::state2:
+            return "state2";
     }
     return "unknown";
 }
 
 static std::string to_string(const event &event) {
     switch (event) {
-        case event::start:
-            return "start";
-        case event::stop:
-            return "stop";
+        case event::event1:
+            return "event1";
+        case event::event2:
+            return "event2";
     }
     return "unknown";
 }
 
 int main() {
-    const auto action1 = []() { std::cout << "action: starting" << std::endl; };
-    const auto action2 = []() { std::cout << "action: stopping" << std::endl; };
+    const auto action1 = []() { std::cout << "action1" << std::endl; };
+    const auto action2 = []() { std::cout << "action2" << std::endl; };
 
     const auto guard1 = []() { return true; };
     const auto guard2 = []() { return false; };
 
     xorz57::transition_table_t<state, event> tt{
-            {{state::idle, event::start}, {guard1, action1, state::running}},
-            {{state::running, event::stop}, {guard1, action2, state::stopped}},
-            {{state::stopped, event::start}, {guard2, action1, state::running}},
+            {{state::state0, event::event1}, {guard1, action1, state::state1}},
+            {{state::state1, event::event2}, {guard1, action2, state::state2}},
+            {{state::state2, event::event1}, {guard2, action1, state::state1}},
     };
 
-    xorz57::state_machine_t<state, event> sm(state::idle, tt);
+    xorz57::state_machine_t<state, event> sm(state::state0, tt);
     std::cout << to_string(sm.get_state()) << std::endl;
 
-    sm.handle_event(event::start);
+    sm.handle_event(event::event1);
     std::cout << to_string(sm.get_state()) << std::endl;
 
-    sm.handle_event(event::stop);
+    sm.handle_event(event::event2);
     std::cout << to_string(sm.get_state()) << std::endl;
 
-    sm.handle_event(event::start);
+    sm.handle_event(event::event1);
     std::cout << to_string(sm.get_state()) << std::endl;
 
     return 0;
